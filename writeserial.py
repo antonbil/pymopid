f2fce1b92f5630b8132aeccabadb82ee2bc562e4
antonbil <anton.bil@hz.nl>
2017-01-15 00:19:41 +0100

    autostart script if Arduino connected

--- writeserial.py
+++ writeserial.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python 
 #sends current status of mpd-player to serial
 #mpd must be running
+"""
+auto-start script if arduino is added:
+in file:
+/etc/udev/rules.d/90-local.rules
+ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="2341", ATTR{idProduct}=="0043", RUN+="/usr/bin/python /home/pi/python/writeserial.py"
 
+reload rules:
+sudo udevadm control --reload-rules
+"""
 import time ## Import 'time' library.  Allows us to use 'sleep' 
 import connectArduino
 import musiccontroller
