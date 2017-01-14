41c1055758f411c452f494c6b696c0b99b877554
antonbil <anton.bil@hz.nl>
2017-01-14 13:40:13 +0100

    new 1602 shield

--- writeserial.py
+++ writeserial.py
@@ -8,32 +8,51 @@ import subprocess
 from itertools import permutations, islice
 import requests
 import json, mpd, musiccontroller
-
+import re
 #main program
 #open serial port
 #port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=0.8)
-port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=0.1, xonxoff=False, rtscts=False, dsrdtr=False)
 #port = serial.Serial("/dev/ttyACM0", timeout=None, baudrate=115000, xonxoff=False, rtscts=False, dsrdtr=False)
 #start main loop to run every second
-line = []
 music_controller=musiccontroller.music_controller()
 
+class ConnectArduino:
+    def __init__(self,music_controller):
+        self.port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=0.1, xonxoff=False, rtscts=False, dsrdtr=False)
+        self.line = []
+        self.music_controller=music_controller
+
+        
+    def exchange(self):
+        status=status=music_controller.get_state()
+
+        #print (status)
+        self.port.write(status['title'])
+        time.sleep(0.02)
+        self.port.write(status['time1'])
+        time.sleep(0.03)
+        for c in self.port.read():
+            if c == '\n':
+                command="".join(self.line)
+                command=re.sub(r'\W+', '', command)
+
+                if command=="Right":
+                    self.music_controller.do_action(0)
+                if command=="Left":
+                    self.music_controller.do_action(3)
+                print("Line: " + command+".")
+                self.line = []
+                break
+            else:
+                self.line.append(c)
+
+
+        #print(time1)
+
+arduino=ConnectArduino(music_controller)
+
 while True:
-    status=status=music_controller.get_state()
-
-    #print (status)
-    port.write(status['title'])
-    time.sleep(0.1)
-    port.write(status['time1'])
-    time.sleep(0.4)
-    for c in port.read():
-        line.append(c)
-        if c == '\n':
-            print("Line: " + "".join(line))
-            line = []
-            break
-
-    #print(time1)
-    time.sleep(0.5)
+    arduino.exchange()
+    time.sleep(0.95)
 port.close()
 
