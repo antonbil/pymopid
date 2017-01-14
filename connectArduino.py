676160ad0485465fcf2e2fc98a316ff960d030cd
antonbil <anton.bil@hz.nl>
2017-01-14 23:18:43 +0100

    ConnectArduino in separate file

---
+++ connectArduino.py
@@ -0,0 +1,71 @@
+import time ## Import 'time' library.  Allows us to use 'sleep' 
+import serial
+import requests
+import mpd
+import re
+from threading import Thread
+
+class Caroussel:
+    def __init__(self,size):
+        self.size=size
+        self.pad=0
+        self.previoustitle=""
+    def gettitle(self,title):
+        if not self.previoustitle==title:
+            self.previoustitle=title
+            self.pad=0
+        else:
+            if len(title)>self.size:
+                title=title+"."
+                self.pad+=1
+                if self.pad>len(title)-1:
+                    self.pad=0
+                title=title[self.pad:]+title[:self.pad]
+        res=title[:self.size].ljust(self.size)
+        return res
+        
+
+class ConnectArduino:
+    def __init__(self,music_controller):
+        try:
+            self.port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=0.1, xonxoff=False, rtscts=False, dsrdtr=False)
+        except:
+            pass
+        self.line = []
+        self.music_controller=music_controller
+        self.title_caroussel=Caroussel(16)
+        self.artist_caroussel=Caroussel(11)
+        self.previoustitle=""
+        self.pad=0
+
+        
+    def exchange(self):
+        status=self.music_controller.get_state()
+        t = Thread(target=self.myfunc, args=(status,self.port,))
+        t.start()
+        return status
+    
+    def myfunc(self,status,port):
+      try:  
+        port.write(status['time1'])
+        time.sleep(0.1)
+        port.write("a"+self.artist_caroussel.gettitle(status['artist']))
+        time.sleep(0.2)
+        port.write("t"+self.title_caroussel.gettitle(status['title']))
+        time.sleep(0.1)
+        for c in port.read():
+            if c == '\n':
+                command="".join(self.line)
+                command=re.sub(r'\W+', '', command)
+
+                if command=="Right":
+                    self.music_controller.do_action(0)
+                if command=="Left":
+                    self.music_controller.do_action(3)
+                #print("Line: " + command+".")
+                self.line = []
+                break
+            else:
+                self.line.append(c)
+      except:
+        pass 
