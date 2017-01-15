cfb08f05b1fd0d41ca73e26b77e0150b39751767
antonbil <anton.bil@hz.nl>
2017-01-15 10:32:49 +0100

    optimize connect to port, switch to send titles

--- connectArduino.py
+++ connectArduino.py
@@ -27,31 +27,39 @@ class Caroussel:
 
 class ConnectArduino:
     def __init__(self,music_controller):
-        try:
-            self.port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=0.1, xonxoff=False, rtscts=False, dsrdtr=False)
-        except:
-            pass
+        self.setPort()
         self.line = []
         self.music_controller=music_controller
         self.title_caroussel=Caroussel(16)
         self.artist_caroussel=Caroussel(11)
         self.previoustitle=""
         self.pad=0
+        self.sendTitle=False
 
         
     def exchange(self):
         status=self.music_controller.get_state()
-        t = Thread(target=self.myfunc, args=(status,self.port,))
-        t.start()
+        try:
+            t = Thread(target=self.myfunc, args=(status,self.port,))
+            t.start()
+        except:
+            self.setPort()
         return status
+    def setPort(self):
+        try:
+            self.port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=0.1, xonxoff=False, rtscts=False, dsrdtr=False)
+        except:
+            pass
+
     
     def myfunc(self,status,port):
       try:  
         port.write(status['time1'])
-        time.sleep(0.1)
-        port.write("a"+self.artist_caroussel.gettitle(status['artist']))
-        time.sleep(0.2)
-        port.write("t"+self.title_caroussel.gettitle(status['title']))
+        if self.sendTitle:
+            time.sleep(0.1)
+            port.write("a"+self.artist_caroussel.gettitle(status['artist']))
+            time.sleep(0.2)
+            port.write("t"+self.title_caroussel.gettitle(status['title']))
         time.sleep(0.1)
         for c in port.read():
             if c == '\n':
@@ -62,10 +70,12 @@ class ConnectArduino:
                     self.music_controller.do_action(0)
                 if command=="Left":
                     self.music_controller.do_action(3)
+                if command=="Title":
+                    self.sendTitle=True
                 #print("Line: " + command+".")
                 self.line = []
                 break
             else:
                 self.line.append(c)
       except:
-        pass 
+        pass
